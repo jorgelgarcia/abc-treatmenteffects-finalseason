905bf60a6621edf8ce747902fb99e87c6a017c10
Joshua Shea <jkcshea@uchicago.edu>
2016-07-04 10:28:38 -0500

    Johsh updated the paths.

--- scripts/abccare/modelselection/paths.py
+++ scripts/abccare/modelselection/paths.py
@@ -4,9 +4,9 @@ from treedict import TreeDict
 filedir = os.path.join(os.path.dirname(__file__))
 
 paths = TreeDict()
-#paths.abccare = os.path.join(os.environ['klmshare'], 'Data_Central', 'Abecedarian', 'data', 'ABC-CARE', 'extensions', 'cba-iv', 'append-abccare_iv.dta')
-paths.abccare = os.path.join(filedir, '..', '..', '..', 'data', 'abccare', 'extensions', 'cba-iv', 'append-abccare_iv.dta') # for testing
-paths.outcomes = os.path.join(filedir, 'outcomes_short.csv')
-paths.instruments = os.path.join(filedir, 'instruments_short.csv')
-paths.controls = os.path.join(filedir, 'controls_short.csv')
+paths.abccare = os.path.join(os.environ['klmshare'], 'Data_Central', 'Abecedarian', 'data', 'ABC-CARE', 'extensions', 'cba-iv', 'append-abccare_iv.dta')
+#paths.abccare = os.path.join(filedir, '..', '..', '..', 'data', 'abccare', 'extensions', 'cba-iv', 'append-abccare_iv.dta') # for testing
+paths.outcomes = os.path.join(filedir, 'outcomes_cba.csv')
+paths.instruments = os.path.join(filedir, 'instruments.csv')
+paths.controls = os.path.join(filedir, 'controls.csv')
 
