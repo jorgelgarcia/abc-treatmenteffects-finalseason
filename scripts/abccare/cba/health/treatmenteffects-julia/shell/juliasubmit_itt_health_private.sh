bef68d3c48d15dc80f980611f2f139d310d16fd6
JessicaYK <yukyungkoh@uchicago.edu>
2016-07-15 14:34:55 -0500

    adding shell files

---
+++ scripts/abccare/cba/health/treatmenteffects-julia/shell/juliasubmit_itt_health_private.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -N Julia
+#PBS -j oe
+#PBS -V
+#PBS -l nodes=1:ppn=20
+
+#-------------------------------------------------
+cd "/home/yukyungkoh/abc-treatmenteffects-finalseason/scripts/abccare/cba/health/treatmenteffects-julia/itt"
+julia parallel_itt_health_private.jl
+#julia abccare_matching.jl
+
+
---
+++ scripts/abccare/cba/health/treatmenteffects-julia/shell/juliasubmit_itt_health_public_surv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -N Julia
+#PBS -j oe
+#PBS -V
+#PBS -l nodes=1:ppn=20
+
+#-------------------------------------------------
+cd "/home/yukyungkoh/abc-treatmenteffects-finalseason/scripts/abccare/cba/health/treatmenteffects-julia/itt"
+julia parallel_itt_health_public_surv.jl
+#julia abccare_matching.jl
+
+
