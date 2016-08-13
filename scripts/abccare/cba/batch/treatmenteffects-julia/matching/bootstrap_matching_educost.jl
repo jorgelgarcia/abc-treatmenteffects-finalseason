50bfc1e7a6652d45b288f36405bc269380cb98f9
JessicaYK <yukyungkoh@uchicago.edu>
2016-08-13 13:21:06 -0500

    further changing some directories in batch

--- scripts/abccare/cba/batch/treatmenteffects-julia/matching/bootstrap_matching_educost.jl
+++ scripts/abccare/cba/batch/treatmenteffects-julia/matching/bootstrap_matching_educost.jl
@@ -18,7 +18,7 @@ global results = "$current/../../rslt"
 global atecode = "$current/../../../../juliafunctions"
 
 # set up number of bootstraps and controls
-global breps = 74 		# remember to subtract 1, i.e. 50 becomes 49
+global breps = 99 		# remember to subtract 1, i.e. 50 becomes 49
 # global areps = 3 	# remember to subtract 1, i.e. 50 becomes 49
 global controls = [:hrabc_index, :apgar1, :apgar5, :hh_sibs0y, :grandma_county, :has_relatives, :male]
 global ipwvars_all = [:m_iq0y, :m_ed0y, :m_age0y, :hrabc_index, :p_inc0y, :apgar1, :apgar5, :prem_birth, :m_married0y, :m_teen0y, :f_home0y, :hh_sibs0y, :cohort, :m_work0y]
@@ -38,7 +38,7 @@ include("$atecode/function/matching.jl")
 include("$atecode/function/ITT.jl")
 
 # Include necessary files
-include("$current/data.jl")
+include("$current/../data.jl")
 
 # ================================================================ #
 # Implement options
