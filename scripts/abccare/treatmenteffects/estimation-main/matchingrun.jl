d25ba236f14730b3f62344b8c1b2bbf30b911add
Jessica Yu Kyung <Jessica Yu Kyung>
2016-09-14 10:54:20 -0500

    changing outcoms_cba file

--- scripts/abccare/treatmenteffects/estimation-main/matchingrun.jl
+++ scripts/abccare/treatmenteffects/estimation-main/matchingrun.jl
@@ -48,7 +48,7 @@ global genderloop = ["male", "female", "pooled"]
 # Bring in data
 # ================================================================ #
 # List of outcomes
-outcomes = readtable("$scripts/../outcomes/outcomes_cba.csv")
+outcomes = readtable("$scripts/../outcomes/outcomes_cba_new.csv")
 
 # ABC/CARE data
 abccare = readtable("$data/append-abccare_iv.csv")
