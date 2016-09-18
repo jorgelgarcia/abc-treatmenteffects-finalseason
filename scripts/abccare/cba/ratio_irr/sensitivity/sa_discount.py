0fd4fafaef374c7838d220301ad334b3a29baf66
aziff <aziff@uchicago.edu>
2016-09-18 15:27:06 -0500

    se for sensitivity

--- scripts/abccare/cba/ratio_irr/sensitivity/sa_discount.py
+++ scripts/abccare/cba/ratio_irr/sensitivity/sa_discount.py
@@ -10,8 +10,8 @@ from copy import deepcopy
 sys.path.extend([os.path.join(os.path.dirname(__file__), '..')])
 filedir = os.path.join(os.path.dirname(__file__))
 plots = os.path.join(filedir, '..', 'rslt', 'sensitivity')
-
 from cba_setup import bc_calc, makeflows
+from cba_N import N
 
 etype = 2
 filled = makeflows(etype=etype)
@@ -20,7 +20,7 @@ filled = makeflows(etype=etype)
 bc_discount = pd.DataFrame([])
 for r in np.arange(0,0.16,0.01) :
 	bc_tmp = deepcopy(filled)
-	output = bc_calc(bc_tmp, rate=r)
+	output = bc_calc(bc_tmp, etype=etype, rate=r)
 	
 	output['rate'] = r
 	bc_discount = pd.concat([bc_discount, output], axis=0)
