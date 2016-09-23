910b6dd7a3abcafa7096a505fa4357de1aea2edd
Anna Ziff <aziff@uchicago.edu>
2016-09-23 18:12:20 -0500

    saving progress on p_inc

--- scripts/abccare/cba/income/parental/load_data.py
+++ scripts/abccare/cba/income/parental/load_data.py
@@ -3,16 +3,13 @@ import sys
 from pandas.io.pytables import read_hdf
 from treedict import TreeDict
 
-sys.path.extend([os.path.join(os.path.dirname(__file__), '..')])
 from paths import paths
 
 extrap = TreeDict()
 
 '''Datasets'''
 
-cnlsy = read_hdf(os.path.join(paths.data, 'data.h5'), 'cnlsy-labor')
 nlsy = read_hdf(os.path.join(paths.data, 'data.h5'), 'nlsy-labor')
 psid = read_hdf(os.path.join(paths.data, 'data.h5'), 'psid-labor')
-interp = read_hdf(os.path.join(paths.data, 'data.h5'), 'interp-labor')
 extrap = read_hdf(os.path.join(paths.data, 'data.h5'), 'extrap-labor')
 abcd = read_hdf(os.path.join(paths.data, 'data.h5'), 'abc-mini')
