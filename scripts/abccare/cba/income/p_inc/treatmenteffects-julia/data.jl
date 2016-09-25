413f7739ba158d4c78c3480168ecf23f2ac7f91d
JessicaYK <yukyungkoh@uchicago.edu>
2016-09-25 16:13:49 -0500

    debugging

--- scripts/abccare/cba/income/p_inc/treatmenteffects-julia/data.jl
+++ scripts/abccare/cba/income/p_inc/treatmenteffects-julia/data.jl
@@ -138,3 +138,6 @@ for dvar in discretized
   abccare[abccare[dvar_p] .> med_d, parse("$(dvar)_dum")] = 1 # Replace dummy column to one if d_var is greater than the median
   abccare[isna(abccare[dvar_p]), parse("$(dvar)_dum")] = NA # Replace values of dunny column if corresponding rwo in original column is NA
 end
+
+println("$(abccare)")
+dcksjnckscndjsnc
