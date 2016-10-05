eda8bd99efb872e17cd1ba17f3410503437eef77
JessicaYK <yukyungkoh@uchicago.edu>
2016-10-05 10:35:14 -0500

    pushing cleaner versions of functions

---
+++ scripts/abccare/juliafunctions/helper/factoranalysis.jl
@@ -0,0 +1,151 @@
+# ========================================================================== #
+# Calculating Factor Scores
+# Author :  Jessica Yu Kyung Koh
+# Date :    09/30/2016
+# Note :    Steps to compute the factor score are the following:
+#            (1) Compute the correlation matrix of the "standardized" data with k independent variables of interest
+#            (2) Compute the eigenvalue + eigenvector pair of the correlation matrix
+#            (3) Choose the number of factors, m (Note: We only use 1 with the max eigenvalue for the ABC-CARE project)
+#                using the value of the eigenvalue (general rule: eigenvalue > 1)
+#            (4) According to the Regression method to compute factor score,
+#                factor score matrix is (m x k) matrix F = [f_{ij}], where
+#                f_{ij} = c_{ij}/sqrt(lambda_j)
+#            (5) Demean independent variables
+#            (6) Compute the dot product of factor score matrix and demeaned independent variables
+#
+# Source: http://www.real-statistics.com/multivariate-statistics/factor-analysis/factor-scores/
+# ========================================================================== #
+
+# ===================================== #
+# Correlation matrix (standardize data) #
+# ===================================== #
+function stdcorrdata(tofactordata)
+
+    datasize = size(tofactordata)
+    N = datasize[1]     # number of rows
+    K = datasize[2]     # number of columns
+
+    # Standardize each measure
+    nonadata = tofactordata[:,:]
+    for k in range(1, K)
+        nonadata[:,k] = (nonadata[:,k] - mean(nonadata[!isna(nonadata[:,k]),k]))/std(nonadata[!isna(nonadata[:,k]),k])
+        nonadata = nonadata[!isna(nonadata[:,k]), :]
+    end
+
+    # Preparation to creating correlation matrix
+    arraydata = Array(nonadata)
+
+    # Compute correlation matrix
+    CM = cor(arraydata)
+    return CM
+end
+
+
+# ===================================================================================== #
+# Number of factors through Scree Plot rule (>1 eigen values of std correlation matrix) #
+# ===================================================================================== #
+function screeplot(tofactordata)
+    datasize = size(tofactordata)
+    N = datasize[1]     # number of rows
+    K = datasize[2]     # number of columns
+    CM = stdcorrdata(tofactordata)
+
+    # Compute eigenvalues, produce scree plot, define number of factors
+    eigCM = eigvals(CM)
+    eigCM = sort!(eigCM, rev=true)    # sort in a descending order
+
+    # Define and display number of factors
+    factorn = max(sum(abs(eigCM) .> 1), 1)
+    return factorn
+end
+
+
+# ======================== #
+# Diagonal factor analysis #
+# ======================== #
+function diagonalfac(sampledata, tofactordata, factorn)
+
+    datasize = size(tofactordata)
+    N = datasize[1]     # number of rows
+    K = datasize[2]     # number of columns
+
+    # Delete rows with NA values and create an array of tofactordata
+    nonadata = tofactordata[:,:]
+    for k in range(1, K)
+        nonadata = nonadata[!isna(nonadata[:,k]), :]
+    end
+
+    arraydata = Array(nonadata)
+    rankdata = rank(arraydata)
+
+    # Generate number of factors
+    if factorn == "None"
+      factorn = screeplot(tofactordata)
+    end
+
+    # Compute standardized correlation matrix
+    CM = stdcorrdata(tofactordata)
+
+    # Create a matrix storing the factor loadings and factor loadings matrices
+    floads  = Array(Float64, K, factorn)
+
+    # Ingredientes to extract factors
+    for j in range(1,factorn)
+        # Calculate the eigenvalue + eigenvector pair of CM
+        eigval = eig(CM)[1]
+        eigvec = eig(CM)[2]
+
+        # Index maximum eigenvalue and choose corresponding eigenvector
+        maxeigval = indmax(eigval)
+        maxeigvec = eigvec[:, maxeigval]
+
+        # Calculate and store factor loadings (eigenvector / sqrt(eigenvalue))
+        floads[:,j] =  maxeigvec ./ sqrt(eigmax(CM))
+    end
+
+    # Demean independent variables in the sample data
+    colnames = names(nonadata)
+    for col in colnames
+      meancol = mean(nonadata[:,col])
+      nonadata[col] -= meancol
+    end
+
+    # compute the factor score ("*" acts like a dot product here)
+    arnondata = Array(nonadata)
+    fscores = arnonadata * floads
+
+    # ------------------------------- #
+    # Add factor score to sample data #
+    # ------------------------------- #
+    # Index rows that have no NA for any of the variables used to create a factor score
+    sampledata[:new_id] = 0
+
+    index = 1
+    for i in 1:N
+      # Turn NA switch on if the row contains at least one NA for any of the variables for factor score
+      naswitch = 0
+      for col in colnames
+        if isna(sampledata[i, col])
+          naswitch = 1
+        end
+      end
+
+      # If naswitch is 0, index the row.
+      if naswitch == 0
+        sampledata[i, :new_id] = index
+        index = index + 1
+      elseif naswitch == 1
+        sampledata[i, :new_id] = NA
+      end
+    end
+
+    # Create new_id for the factor score
+    fscoresize = size(fscore)[1]
+    fscoreframe = DataFrame(new_id = 1:fscoresize, factor = fscore)
+
+    # Merge factorscore using new_id
+    mergedata = join(sampledata, fscoreframe, on = [:new_id], kind = :outer)
+    delete!(mergedata, :new_id)
+
+    return mergedata
+end
--- scripts/abccare/juliafunctions/helper/factors.jl
+++ scripts/abccare/juliafunctions/helper/factors.jl
@@ -2,7 +2,7 @@
 # Factor Analysis for ABC-CARE Treatment Effects
 # Author: Joshua Shea
 # Translator: Jessica Yu Kyung Koh
-# Date: 08/04/2016
+# Date: 10/03/2016
 # ============================================================================ #
 
 # ================================================================ #
@@ -10,41 +10,38 @@
 # ================================================================ #
 factors = Dict()
 
-factors["factor_iq5"] = [:iq2y, :iq3y, :iq3y6m, :iq4y, :iq4y6m, :iq5y]
-factors["factor_iq12"] = [:iq7y, :iq8y, :iq12y]
-factors["factor_iq21"] = [:iq15y, :iq21y]
-factors["factor_achv12"] = [:ach5y6m, :ach6y, :ach6y6m, :ach7y, :ach7y6m, :ach8y, :ach8y6m, :piat_math7y]
-factors["factor_achv21"] = [:ach15y, :ach21y]
+factors["factor_iq5y"] = [:iq2y, :iq3y, :iq3y6m, :iq4y, :iq4y6m, :iq5y]
+factors["factor_iq12y"] = [:iq7y, :iq8y, :iq12y]
+factors["factor_iq21y"] = [:iq15y, :iq21y]
+factors["factor_ach12y"] = [:ach5y6m, :ach6y, :ach6y6m, :ach7y, :ach7y6m, :ach8y, :ach8y6m, :piat_math7y]
+factors["factor_ach21y"] = [:ach15y, :ach21y]
 factors["factor_home"] = [:home0y6m, :home1y6m, :home2y6m, :home3y6m, :home4y6m ,:home8y]
-factors["factor_pinc"] = [:p_inc1y6m, :p_inc2y6m, :p_inc3y6m, :p_inc4y6m, :p_inc8y, :p_inc12y, :p_inc15y]
+factors["factor_pinc"] = [:p_inc1y6m, :p_inc2y6m, :p_inc3y6m, :p_inc4y6m, :p_inc8y, :p_inc12y, :p_inc15y, :p_inc21y]
 factors["factor_mwork"] = [:m_work1y6m, :m_work2y6m, :m_work3y6m, :m_work4y6m, :m_work21y]
-factors["factor_meduc"] = [:mb_ed1y6m, :mb_ed2y6m, :mb_ed3y6m, :mb_ed4y6m, :mb_ed8y]
+# factors["factor_meduc"] = [:mb_ed1y6m, :mb_ed2y6m, :mb_ed3y6m, :mb_ed4y6m, :mb_ed8y]
 factors["factor_fhome"] = [:f_home1y6m, :f_home2y6m, :f_home3y6m, :f_home4y6m, :f_home8y]
 factors["factor_educ"] = [:sch_hs30y, :si30y_techcc_att, :si30y_univ_comp, :years_30y]
-factors["factor_emp"] = [:si30y_works_job, :si21y_inc_labor, :si30y_inc_labor, :si21y_inc_trans_pub, :si30y_inc_trans_pub]
+factors["factor_emp"] = [:si30y_works_job, :si21y_inc_labor, :si30y_inc_labor, :inv_si21y_inc_trans_pub, :inv_si30y_inc_trans_pub]
 factors["factor_crime"] = [:ad34_fel, :ad34_mis, :si30y_adlt_totinc]
 factors["factor_tad"] = [:si30y_cig_num, :drink_days, :drink_binge_days, :si34y_drugs]
 factors["factor_shealth"] = [:si30y_subj_health, :si34y_subj_health]
 factors["factor_hyper"] = [:si34y_sys_bp, :si34y_dia_bp, :si34y_prehyper, :si34y_hyper]
-factors["factor_chol"] = [:si34y_chol_hdl, :si34y_dyslipid]
+factors["factor_chol"] = [:inv_si34y_chol_hdl, :si34y_dyslipid]
 factors["factor_diabetes"] = [:si34y_hemoglobin, :si34y_prediab, :si34y_diab]
 factors["factor_obese"] = [:si34y_bmi, :si34y_obese, :si34y_sev_obese, :si34y_whr, :si34y_obese_whr, :si34y_fram_p1]
-factors["factor_bsi"] = [:bsi_tsom, :BSISom_T, :bsi_tdep, :BSIDep_T, :bsi_tanx, :BSIAnx_T, :bsi_thos, :BSIHos_T, :bsi_tgsi, :B18GSI_T, :bsi_rsom, :BSISomRw, :bsi_rdep, :BSIDepRw, :bsi_ranx, :BSIAnxRw, :bsi_rhos, :BSIHosRw]
+factors["factor_bsi"] = [:bsi_tsom, :BSISom_T, :bsi_tdep, :BSIDep_T, :bsi_tanx, :BSIAnx_T, :bsi_thos, :BSIHos_T, :bsi_tgsi, :B18GSI_T]
 
 # ================ #
 # Data preparation #
 # ================ #
-# Generate a column with count of individuals (bootstrap samples may have multiple same id's)
-sampledata[:new_id] = [1:size(sampledata,1)]
-
 # Flip signs of some variables
 flip_variables = [:si34y_chol_hdl, :si21y_inc_trans_pub, :si30y_inc_trans_pub]
 for var in flip_variables
-	sampledata[var] = sampledata[var] .* -1
+	sampledata[parse("inv_$(var)")] = sampledata[var] .* -1
 end
 
 # Create a list of all categories
-categories = [:iq5, :iq12, :iq21, :achv12, :achv21, :home, :pinc, :mwork, :meduc, :fhome, :educ, :emp, :crime, :tad, :shealth, :hyper, :chol, :diabetes, :obese, :bsi]
+categories = [:iq5y, :iq12y, :iq21y, :ach12y, :ach21y, :home, :pinc, :mwork, :meduc, :fhome, :educ, :emp, :crime, :tad, :shealth, :hyper, :chol, :diabetes, :obese, :bsi]
 
 # Determine if we need to deal with ABC
 if size(sampledata[sampledata[:abc] .== 1, :], 1) > 0
@@ -90,20 +87,20 @@ for cat in categories
 				new_cat_local = push!(new_cat_local, var)
 			end
 		end
+  end
 
-		factordata["factor_$(cat)"] = sampledata[:, new_cat_local]
+	factordata["factor_$(cat)"] = sampledata[:, new_cat_local]
 
-	# Calculate the number of factors using a defined function
-		factor_switch = 1
-		try
-			diagonalfac(factor["factor_$(cat)"], 1)
-		catch err
-			factor_switch = 0
-		end
+# Calculate the number of factors using a defined function
+	factor_switch = 1
+	try
+		diagonalfac(sampledata, factordata["factor_$(cat)"], 1)
+	catch err
+		factor_switch = 0
+	end
 
-		if factor_switch = 1
-			diagonalfac(factor["factor_$(cat)"], 1)
-
-		end
+	if factor_switch = 1
+		sampledata = diagonalfac(sampledata, factordata["factor_$(cat)"], 1)
+    rename!(sampledata, :factor, parse("factor_$(cat)"))
 	end
 end
