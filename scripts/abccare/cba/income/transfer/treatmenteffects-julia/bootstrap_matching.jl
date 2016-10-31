f506488454853798475594d098ae48f0e8fe5a09
JessicaYK <yukyungkoh@uchicago.edu>
2016-10-31 16:10:05 -0500

    changing labels for transfer income factor switch

--- scripts/abccare/cba/income/transfer/treatmenteffects-julia/bootstrap_matching.jl
+++ scripts/abccare/cba/income/transfer/treatmenteffects-julia/bootstrap_matching.jl
@@ -24,7 +24,7 @@ global breps = 98 		# remember to subtract 1, i.e. 50 becomes 49
 global areps = 98   	# remember to subtract 1, i.e. 50 becomes 49
 global controls = [:hrabc_index, :apgar1, :apgar5, :hh_sibs0y, :grandma_county, :has_relatives, :male, :abc]
 global ipwvars_all = [:m_iq0y, :m_ed0y, :m_age0y, :hrabc_index, :p_inc0y, :apgar1, :apgar5, :prem_birth, :m_married0y, :m_teen0y, :f_home0y, :hh_sibs0y, :cohort, :m_work0y, :has_relatives]
-global factors = 0
+global factorswitch = 0
 global deaths = 1
 
 # Include helper files
