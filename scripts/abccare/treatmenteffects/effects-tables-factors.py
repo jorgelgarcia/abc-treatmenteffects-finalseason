767e37e20d0ebd02a6c5d75d7be84d80da0c428d
JessicaYK <yukyungkoh@uchicago.edu>
2016-11-19 16:43:02 -0600

    turning off ts switch for factors

--- scripts/abccare/treatmenteffects/effects-tables-factors.py
+++ scripts/abccare/treatmenteffects/effects-tables-factors.py
@@ -28,7 +28,7 @@ path_results = os.path.join(filedir, 'rslt-appendix/')
 path_outcomes = os.path.join(filedir, '../outcomes/outcomes_cba_factors.csv')
 
 # provide option for two sided tests
-twosided = 1
+twosided = 0
 
 
 #=========================================
