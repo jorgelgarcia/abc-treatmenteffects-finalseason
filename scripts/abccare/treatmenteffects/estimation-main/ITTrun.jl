40f6362ed5e88615f28381fc5e173044479ee389
JessicaYK <yukyungkoh@uchicago.edu>
2016-11-27 20:05:50 -0600

    changing the order of gender

--- scripts/abccare/treatmenteffects/estimation-main/ITTrun.jl
+++ scripts/abccare/treatmenteffects/estimation-main/ITTrun.jl
@@ -66,7 +66,7 @@ d_index = 1
 
 ITTini = Dict()
 
-for data in ("abccare", "abc", "care")
+for data in ("abccare", "abc")
 	if data == "abccare"
 		datainuse = abccare_data
 		controlset = conDict["controls_abccare"]
@@ -107,7 +107,7 @@ function ITTrun(boots)
 
 	d_index = 1
 
-	for data in ("abccare", "abc", "care")
+	for data in ("abccare", "abc")
 		if data == "abccare"
 			datainuse = abccare_data
 			controlset = conDict["controls_abccare"]
